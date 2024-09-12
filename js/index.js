8c6045a9423022b68d02735c4dfa39e7b4047b72
NoorulAyaan <Ayan.cloudlem@gmail.com>
2024-09-12 19:25:46 +0000

    Added date

--- js/index.js
+++ js/index.js
@@ -8,22 +8,52 @@
 // document.getElementById("demo").innerHTML = typeof a
 
 
-let a = prompt("Hey what's your age")
-a = Number.parseInt(a)  //parseint() converts a string into a number
+// let a = prompt("Hey what's your age")
+// a = Number.parseInt(a)  //parseint() converts a string into a number
 
-if (a < 33) {
-    // alert("This is a valid age")
-    document.getElementById("demo").innerHTML = "You are fail";
+// if (a < 33) {
+//     // alert("This is a valid age")
+//     document.getElementById("demo").innerHTML = "You are fail";
+// }
+// else if (a > 60 && a < 70){
+//     document.getElementById("demo").innerHTML = "You have scored B grade";
+// }
+// else if(a > 75 && a < 80){
+//     document.getElementById("demo").innerHTML = "You have scored A- grade"
+// }
+// else if(a <= 100 && a > 80){
+//     document.getElementById("demo").innerHTML = "You have A+ grade";
+// }
+// else{
+//     document.getElementById("demo").innerHTML = "Invalid number";  
+// }
+
+
+function myFunction(){
+    let day;
+    switch (new Date().getDay()) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Satureday"
+    }
+    onclick = document.getElementById("demo1").innerHTML = "Today is " + day
 }
-else if (a > 60 && a < 70){
-    document.getElementById("demo").innerHTML = "You have scored B grade";
-}
-else if(a > 75 && a < 80){
-    document.getElementById("demo").innerHTML = "You have scored A- grade"
-}
-else if(a <= 100 && a > 80){
-    document.getElementById("demo").innerHTML = "You have A+ grade";
-}
-else{
-    document.getElementById("demo").innerHTML = "Invalid number";  
-}
+
+
